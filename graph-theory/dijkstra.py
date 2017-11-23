3c39349e2f1cc6b1ab4d9555b3bb2e08413fe8f4
Xxiong <xxiong@cqu.edu.cn>
2017-11-23 22:38:45 +0800

    dijkstra: add undirected graph loadfunction

--- graph-theory/dijkstra.py
+++ graph-theory/dijkstra.py
@@ -1,8 +1,28 @@
 #!/bin/python2.7
+import sys
 
-FILENAME = "graph.txt"
+## 无向图
+def load_ungraph(filename):
+    cnt = 0
+    symbo = dict()
+    graph = list()
+    with open(filename, "rt") as f:
+        for i in f:
+            ids = i.strip("\r\n").split(",")
+            if ids[0] not in symbo.keys():
+                symbo[ids[0]] = cnt
+                cnt += 1
+            if ids[1] not in symbo.keys():
+                symbo[ids[1]] = cnt
+                cnt += 1
+            while len(graph) < cnt:
+                graph.append([])
+            graph[symbo[ids[0]]].append([symbo[ids[1]], int(ids[2])])
+            graph[symbo[ids[1]]].append([symbo[ids[0]], int(ids[2])])
+    return symbo, graph
 
-def loadgraph(filename):
+## 有向图
+def load_digraph(filename):
     cnt = 0
     symbo = dict()
     graph = list()
@@ -21,10 +41,14 @@ def loadgraph(filename):
     return symbo, graph
 
 
+def dijkstra(symbo, graph, startpos):
+    print symbo
+    print graph
 
-def dijkstra(filename=None):
-    pass
 
 
 if __name__ == "__main__":
-    loadgraph("graph.txt")
+    filename = "graph.txt"
+    startpos = "beijing"
+    symbo, graph = load_ungraph(filename)
+    dijkstra(symbo, graph, startpos)
